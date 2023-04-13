dcbd4848b07dfb4dfc38fa2462f6c14dcbdd1f70
jacinta25 <mwendejacinta25@gmail.com>
2023-04-12 23:19:23 -0700

    argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the name of a program
+ * @argc:unused parameter
+ * @argv: contains the arguements to be passed
+ *
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
